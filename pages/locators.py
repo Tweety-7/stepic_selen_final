2230cc392fbf74a846b48897c0df605aadbad827
Arina <arinkka7@mail.ru>
2020-10-28 12:09:23 +0300

    locators ok

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+# class LoginPageLocators(B)
+# 	def __init__(self):
+# 		pass
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
