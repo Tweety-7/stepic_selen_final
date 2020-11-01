29a590c1059493419870ca8c95fc42b6bc176793
Arina <arinkka7@mail.ru>
2020-11-01 22:32:03 +0300

    pytest.ini added

--- test_product_page.py
+++ test_product_page.py
@@ -7,23 +7,24 @@ from pages.login_page import LoginPage
 import time
 	# @pytest.mark.parametrize(promo_code, range(10))
 # 	@pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='')) for i in range(10)])
-# @pytest.mark.xfail
+@pytest.mark.xfail
+@pytest.mark.need_review
 # @pytest.mark.parametrize('promo_offer', ["0","1","2", "3", "4", "5", "6", "7", "8", "9"])
-# def test_guest_can_add_product_to_basket(browser):
-# 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-# 	# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-# 	    # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-# 	# link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
-# 	page = PageObject(browser, link)
-# 	page.open()
-# 	page.shoud_be_btn()
-# 	name = browser.find_element(*ProductPageLocators.NAME_BOOK)
-# 	price = browser.find_element(*ProductPageLocators.PRICE_BOOK)
-# 	name = name.text
-# 	price = price.text
-# 	page.click_to_add()
-# 	page.solve_quiz_and_get_code()
-# 	page.add_is_ok(name, price)
+def test_guest_can_add_product_to_basket(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+	# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	    # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+	# link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+	page = PageObject(browser, link)
+	page.open()
+	page.shoud_be_btn()
+	name = browser.find_element(*ProductPageLocators.NAME_BOOK)
+	price = browser.find_element(*ProductPageLocators.PRICE_BOOK)
+	name = name.text
+	price = price.text
+	page.click_to_add()
+	page.solve_quiz_and_get_code()
+	page.add_is_ok(name, price)
 # @pytest.mark.xfail
 # def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 # 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -56,24 +57,26 @@ import time
 # 	page = PageObject(browser, link)
 # 	page.open()
 # 	page.should_be_login_link()
-# # @pytest.mark.xfail
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-# 	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-# 	page = PageObject(browser, link)
-# 	page.open()
-# 	page.go_to_login_page()
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-# 	# Гость открывает страницу товара
-# 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-# 	page = PageObject(browser, link)
-# 	page.open()
-# 	# Переходит в корзину по кнопке в шапке
-# 	basket_page = page.go_to_basket()
-# 	basket_page = BasketPage(browser, browser.current_url)
-# 	# Ожидаем, что в корзине нет товаров
-# 	basket_page.should_be_basket_is_empty()
+@pytest.mark.xfail
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = PageObject(browser, link)
+	page.open()
+	page.go_to_login_page()
+@pytest.mark.need_review	
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+	# Гость открывает страницу товара
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+	page = PageObject(browser, link)
+	page.open()
+	# Переходит в корзину по кнопке в шапке
+	basket_page = page.go_to_basket()
+	basket_page = BasketPage(browser, browser.current_url)
+	# Ожидаем, что в корзине нет товаров
+	basket_page.should_be_basket_is_empty()
 	# Ожидаем, что есть текст о том что корзина пуста 
-	# basket_page.basket_text_empty()
+	basket_page.basket_text_empty()
 @pytest.mark.user
 class TestUserAddToBasketFromProductPage():
 	@pytest.fixture(scope="function", autouse=True)
@@ -91,6 +94,7 @@ class TestUserAddToBasketFromProductPage():
 		page.open()
 		# Проверяем, что нет сообщения об успехе с помощью is_not_element_present
 		assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE)
+	@pytest.mark.need_review
 	def test_user_can_add_product_to_basket(self, browser):
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 		# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
